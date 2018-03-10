f7c26a51cb1212225028b9ea9da916a88e1ed70e
lionel-rigoux <lionel.rigoux@sf.mpg.de>
2018-03-10 16:28:57 +0100

    minimal working version

---
+++ spec/pandemic-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PandemicView from '../lib/pandemic-view';
+
+describe('PandemicView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
